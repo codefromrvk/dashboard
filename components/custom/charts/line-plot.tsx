492f65b490e1014613cc236108953fbc0b670d06
codefromrvk <varshithkumar553@gmail.com>
2023-11-20 17:16:21 +0530

    refactor: remove comments ++

--- components/custom/charts/line-plot.tsx
+++ components/custom/charts/line-plot.tsx
@@ -6,7 +6,6 @@ import { useRef, useEffect, useState } from "react";
 
 export default function LinePlot({
   data,
-  lineChartData,
   width = 440,
   height = 240,
   marginTop = 20,
@@ -22,14 +21,10 @@ export default function LinePlot({
     const x = d3.scaleTime().range([0, width]);
     const y = d3.scaleLinear().range([height, 0]);
     const y2 = d3.scaleLinear().range([height, 0]);
-    // const y3 = d3.scaleLinear().range([height, 0]);
 
     x.domain(d3.extent(data, (d) => d.date));
-    // y.domain([0, d3.max(data, (d) => d.confirmed)]);
     y.domain([
       0,
-      // d3.max(data, (d) => d.deceased),
-
       Math.max(
         d3.max(data, (d) => d.confirmed),
         d3.max(data, (d) => d.recovered)
@@ -40,12 +35,10 @@ export default function LinePlot({
       d3.max(data, (d) => d.deceased),
     ]);
 
-    // y3.domain([0, d3.max(data, (d) => d.valueB)])  ;
 
     svg.select(".x-axis").call(
       d3
         .axisBottom(x)
-        // .ticks(d3.timeDay.every(10))
         .tickFormat(d3.timeFormat("%d %b"))
     ).selectAll("text")
     .style("text-anchor", "end")
@@ -58,14 +51,12 @@ export default function LinePlot({
       .select(".yA-axis")
       .call(d3.axisRight(y2))
       .attr("transform", `translate(${width}, 0)`);
-    // svg.select(".yB-axis").call(d3.axisLeft(y3));
 
     const valueLine = d3
       .line()
       .x((d) => x(d.date))
       .y((d) => y(d.confirmed))
       .curve(d3.curveBasis);
-      // const line = d3.line((d, i) => x(i), y)
 
     const valueLineA = d3
       .line()
@@ -85,20 +76,7 @@ export default function LinePlot({
     svg.select(".lineA").datum(data).attr("d", valueLineA);
     svg.select(".lineB").datum(data).attr("d", valueLineB);
 
-    // const gradient = svg
-    //   .append("defs")
-    //   .append("linearGradient")
-    //   .attr("id", "axisGradient")
-    //   .attr("x1", "0%")
-    //   .attr("y1", "0%")
-    //   .attr("x2", "0%")
-    //   .attr("y2", "100%");
-
-    // gradient.append("stop").attr("offset", "0%").style("stop-color", "green");
-
-    // gradient.append("stop").attr("offset", "100%").style("stop-color", "red");
-
-    // svg.select(".y-axis").style("fill", "url(#axisGradient)");
+    
   }, [data, height, width]);
 
   return (
@@ -114,7 +92,6 @@ export default function LinePlot({
         />
         <g className="y-axis multi-axis-line" />
         <g className="yA-axis multi-axis-line" />
-        {/* fill="steelblue" */}
         <path className="line" fill="none" stroke="red" strokeWidth="1.5" />
         <path className="lineA" fill="none" stroke="green" strokeWidth="1.5" />
         <path
@@ -135,31 +112,6 @@ export default function LinePlot({
         />
       </g>
     </svg>
-  );
-  return (
-    <div className="px-4">
-      <svg width={width - marginBottom} height={height}>
-        <g
-          ref={gx}
-          fontSize={10}
-          transform={`translate(0,${height - marginBottom + 10})`}
-          color="transparent"
-          className="axis-line"
-        />
-        <g
-          ref={gy}
-          transform={`translate(${marginLeft},0)`}
-          color="transparent"
-          className="axis-line"
-        />
-        <path
-          fill="none"
-          stroke="#16a34a"
-          strokeWidth="3"
-          d={line(data)?.toString()}
-        />
-      </svg>
-    </div>
   );
 }
 
